5a5fb6dc093ce16715dbe93b9ed20105fe3eb987
MOHAMED SHEHAS V <mohamedshehas.v2@mindtree.com>
2021-01-13 13:42:27 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  notifyMe() {
+
+  if (!("Notification" in window)) {
+    console.log("This browser does not support desktop notification");
+  }
+
+
+  else if (Notification.permission === "granted") {
+
+    var notification = new Notification("Hi there!");
+  }
+
+
+  else if (Notification.permission !== 'denied' ) {
+    Notification.requestPermission(function (permission) {
+  
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+
+
+}
+
 }
